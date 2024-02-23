e9064a483a84268b8173c74f709ea16682f94096
dylanP97 <dylan.pinheiro.1997@gmail.com>
2024-02-23 16:45:38 +0100

    adding api url

--- app/components/Connexion.jsx
+++ app/components/Connexion.jsx
@@ -19,11 +19,11 @@ const Connexion = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log("http://localhost:5000")
+    console.log("https://node-wolvesville.onrender.com")
 
     if (isLogin) {
       try {
-        const response = await fetch("http://localhost:5000" + "/api/user/login", {
+        const response = await fetch("https://node-wolvesville.onrender.com" + "/api/user/login", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -39,7 +39,7 @@ const Connexion = () => {
       }
     } else {
       try {
-        const response = await fetch("http://localhost:5000" + "/api/user/signup", {
+        const response = await fetch("https://node-wolvesville.onrender.com" + "/api/user/signup", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
