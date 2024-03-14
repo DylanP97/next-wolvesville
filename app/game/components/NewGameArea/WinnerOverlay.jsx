b3ea9c6ab818b972e26723590206781505281688
dylanP97 <dylan.pinheiro.1997@gmail.com>
2024-03-14 17:04:38 +0100

    adding content to WinnerOverlay

--- app/game/components/NewGameArea/WinnerOverlay.jsx
+++ app/game/components/NewGameArea/WinnerOverlay.jsx
@@ -3,6 +3,7 @@
 import { Button } from "@nextui-org/react";
 import Image from "next/image";
 import { useAuth } from "../../../providers/AuthProvider";
+import AvatarUI from "../../../profile/Profile/AvatarUI";
 
 const WinnerOverlay = ({ winningTeam }) => {
   const { updateGameState } = useAuth();
@@ -12,8 +13,6 @@ const WinnerOverlay = ({ winningTeam }) => {
     document.location.assign('/')
   }
 
-  console.log(winningTeam)
-
   return (
     <div
       className="winner-overlay"
@@ -21,22 +20,31 @@ const WinnerOverlay = ({ winningTeam }) => {
         zIndex: 999,
       }}>
       <div className="winner-message flex flex-col justify-center align-center">
-        <Image src={winningTeam.image} alt="winner" height={200} width={200} className="m-4" />
-        <p>The {winningTeam.name} won!</p>
-        <div className="flex flex-col justify-center">
+        <Image src={winningTeam.image} alt="winner" height={80} width={80} className="mb-2 w-full max-h-16 object-contain" />
+        <p className="mb-4">The {winningTeam.name} won!</p>
+        <div className="flex flex-row justify-center mt-4">
           {
-            winningTeam.alivePlayers.map((p) => {
-              <div key={p.name + 'key-win-div'} className="flex flex-row items-center">
-                <p>{p.name} as </p>
-                <Image
-                  src={p.role.image}
-                  alt={p.role.name + "win-icon-76678"}
-                  height={100}
-                  width={100}
-                  style={{ height: "auto", width: "auto" }}
-                  className="h-12 w-12"
-                />
-              </div>
+            winningTeam.aliveList.map((ply) => {
+              return (
+                <div key={ply.name + 'key-win-div'} className="flex flex-col items-center w-44">
+                  <div className="flex relative">
+                    <Image
+                      src={ply.role.image}
+                      alt={ply.role.name + "win-icon-76678"}
+                      height={40}
+                      width={40}
+                      style={{ height: "auto", width: "auto" }}
+                      className="p-2 absolute top-[-15%] left-[55%] "
+                    />
+                    <div className="p-2 bg-blue-500 rounded-full ">
+                      <AvatarUI
+                        avatar={ply.avatar} heightAndWidth={80}
+                      />
+                    </div>
+                  </div>
+                  <p className="m-2 text-gray-200 text-sm text-clip italic">{ply.name} as {ply.role.name}</p>
+                </div>
+              )
             })
           }
         </div>
